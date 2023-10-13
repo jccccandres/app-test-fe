4b125845bf6e8e6ffd21c19fb321bd0c420c73ec
jcandres <jc.andres17@gmail.com>
2023-10-13 10:20:06 +0800

    add insert user page

--- src/api/User.tsx
+++ src/api/User.tsx
@@ -4,4 +4,12 @@ const getUsers = async () => {
   return await axiosInstance.get("users");
 };
 
-export { getUsers };
+const addUser = async (data: {
+  name: string;
+  email: string;
+  roles: string[];
+}) => {
+  return await axiosInstance.post("users", data);
+};
+
+export { getUsers, addUser };
--- src/pages/Users/AddUsers.tsx
+++ src/pages/Users/AddUsers.tsx
@@ -1,30 +1,167 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { addUser } from "../../api/User";
 
 const Add = () => {
-  const [fullname, setFullName] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [roles, setRoles] = useState<string[]>([]);
+  const navigate = useNavigate();
 
   const handleChangeFullName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFullName(e.target.value);
+    setName(e.target.value);
   };
 
   const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
 
+  const handleCheckBox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setRoles([...roles, e.target.value]);
+    } else {
+      let temp = roles;
+      temp.splice(roles.indexOf(e.target.value), 1);
+      setRoles(temp);
+    }
+  };
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    addUser({ name, email, roles })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 201) {
+          navigate("/view-users");
+        } else {
+          alert(response.data.message);
+        }
+      })
+      .catch((error) => {
+        alert(error.response.data.message);
+        console.log(error);
+      });
+  };
+
+  const defaultRoles = [
+    {
+      id: 1,
+      description: "Author",
+    },
+    {
+      id: 2,
+      description: "Editor",
+    },
+    {
+      id: 3,
+      description: "Subscriber",
+    },
+    {
+      id: 4,
+      description: "Administrator",
+    },
+  ];
+
   return (
-    <div>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        margin: "10px 30px",
+      }}
+    >
+      <Link to="/">
+        <button
+          style={{
+            margin: "5px",
+            padding: "5px 10px",
+            fontSize: "20px",
+            cursor: "pointer",
+          }}
+        >
+          Back
+        </button>
+      </Link>
       <h2>Add User</h2>
 
-      <div>
-        <label>Full Name</label>
-        <input type="text" value={fullname} onChange={handleChangeFullName} />
-      </div>
+      <form
+        onSubmit={handleFormSubmit}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "5px",
+          }}
+        >
+          <label style={{ marginBottom: "5px" }}>Full Name</label>
+          <input
+            type="text"
+            style={{ fontSize: "23px", padding: "5px 10px" }}
+            value={name}
+            onChange={handleChangeFullName}
+          />
+        </div>
 
-      <div>
-        <label>Email</label>
-        <input type="text" value={email} onChange={handleChangeEmail} />
-      </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "10px 5px",
+          }}
+        >
+          <label style={{ marginBottom: "5px" }}>Email</label>
+          <input
+            type="text"
+            style={{ fontSize: "23px", padding: "5px 10px" }}
+            value={email}
+            onChange={handleChangeEmail}
+          />
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "10px 5px",
+          }}
+        >
+          <label style={{ marginBottom: "5px" }}>Roles</label>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            {defaultRoles.map((role) => {
+              return (
+                <label
+                  key={role.id}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    cursor: "pointer",
+                  }}
+                >
+                  <input
+                    type="checkbox"
+                    style={{ width: "20px", height: "20px" }}
+                    value={role.id}
+                    onChange={handleCheckBox}
+                  />
+                  <span>{role.description}</span>
+                </label>
+              );
+            })}
+          </div>
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          <button
+            type="submit"
+            style={{ padding: "5px 10px", fontSize: "17px", cursor: "pointer" }}
+          >
+            Save
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
